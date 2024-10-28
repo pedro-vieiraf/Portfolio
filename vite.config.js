b106cb1caecd4068617ce96a9b0c02d859a690df
Pedro Vieira <pedrovieirafdev@gmail.com>
2024-10-28 15:43:28 -0300

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/Portfolio',
   plugins: [react()],
   css: {
     modules: {
